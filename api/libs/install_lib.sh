7fdf771c19e3110ca03cbca280d11b844d0c146a
Julien Martin <balteo@gmail.com>
2019-09-08 15:50:49 +0200

    Remove libs/ directory with jar & install script

--- api/libs/install_lib.sh
+++
@@ -1,2 +0,0 @@
-mvn install:install-file -Dfile=./spring-data-r2dbc-1.0.0.BUILD-SNAPSHOT.jar -DgroupId=org.springframework.data \
-    -DartifactId=spring-data-r2dbc -Dversion=1.0.0.BUILD-SNAPSHOT -Dpackaging=jar
