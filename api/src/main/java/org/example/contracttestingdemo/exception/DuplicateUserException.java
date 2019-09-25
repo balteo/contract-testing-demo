409687f4a848427a0c4c7080d134c47bb3bbed6f
Julien Martin <balteo@gmail.com>
2019-09-25 08:45:06 +0200

    Refactor UserHandler to rely on Mono.error and custom exceptions

--- api/src/main/java/org/example/contracttestingdemo/exception/DuplicateUserException.java
+++ api/src/main/java/org/example/contracttestingdemo/exception/DuplicateUserException.java
@@ -3,14 +3,11 @@ package org.example.contracttestingdemo.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.validation.ObjectError;
-
-import java.util.List;
 
 @AllArgsConstructor
-public class ValidationException extends RuntimeException {
+public class DuplicateUserException extends RuntimeException {
 
     @Getter
     @Setter
-    private List<ObjectError> errors;
+    private String errorMessage;
 }
